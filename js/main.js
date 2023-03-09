3bae65be1c15e876c5e079ee3641b71e5e18a37a
wiphodfx <wifidifx@gmail.com>
2023-03-09 04:46:05 +0100

    Swiper prev and next styling

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    // el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
