6449693244c0b0ff5d6e27ee57783db9054428dc
wiphodfx <wifidifx@gmail.com>
2023-03-09 16:47:35 +0100

    Media querie set for explore section and search bar

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// ----------------------------ON SCROLL SLIDE IN----------------------------
+
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveals");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var reavealTop = reveals[i].getBoundingClientRect().top;
+
+    var reavealPoint = 150;
+
+    if (reavealTop < windowHeight - reavealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      // reveals[i].classList.remove("active");
+    }
+  }
+}
